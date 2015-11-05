373faad4abd373e541b414cfe67af3125403f024
Jan Kučera <kucera.jan.cz@gmail.com>
2015-11-05 20:57:35 +0100

    Field Factories
    Added: BooleanFactory

--- src/main/java/org/esbench/generator/document/DocumentFactory.java
+++ src/main/java/org/esbench/generator/document/DocumentFactory.java
@@ -1,14 +1,14 @@
 package org.esbench.generator.document;
 
 /**
- * Factory for creating new instances of JSON document. 
+ * Factory for creating new instances of JSON document.
  * @param <T> defines return type of JSON document
  */
 public interface DocumentFactory<T> {
 
   /**
    * Create new instance of JSON document.
-   * @param instanceId used for generating event's data.  
+   * @param instanceId used for generating event's data.
    * @return T document instance
    */
   public T newInstance(int instanceId);
